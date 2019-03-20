b9d63cdb3d3add05060bdfd46d32672969a06404
luka-mladenovic <luka.mladenovic@enkronos.com>
2019-03-20 10:45:37 +0100

    Update aliases

--- user-aliases.cmd
+++ user-aliases.cmd
@@ -1,44 +1,42 @@
-;= @echo off
-;= rem Call DOSKEY and use this file as the macrofile
-;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
-;= rem In batch mode, jump to the end of the file
-;= goto:eof
-;= Add aliases below here
-e.=explorer .
-history=cat %CMDER_ROOT%\config\.history
-unalias=alias /d $1
-ls=ls --show-control-chars -F --color $*
-pwd=cd
-clear=cls
-touch=echo $null >> $*
+:: Artisan commands
+tinker=php artisan tinker
 phpmig=php artisan migrate
 phpref=php artisan migrate:refresh
+phpfresh=php artisan migrate:fresh
 phpreset=php artisan migrate:reset
 phproll=php artisan migrate:rollback $*
 phpseed=php artisan db:seed
-phpseedx=php artisan db:seed --class=$*
-makemod=php artisan make:model $* -m -c && composer dump-autoload
-makemig=php artisan make:migration $* && composer dump-autoload
 cdump=composer dump-autoload
+:: Testing
 glt=gulp test
 glw=gulp watch
+nah=git checkout .
 unit=phpunit
-univ=phpunit --verbose
-unid=phpunit --debug
-unig=phpunit --group $*
-unif=phpunit --filter $*
-tinker=php artisan tinker
+group=phpunit --group=$*
+filter=phpunit --filter=$*
+suite=phpunit --testsuite=$*
+:: Commit
 g=git $*
 gs=git status
 gss=git status -s
 ga=git add $*
-gaa=git add -A
-gc=git commit    
+gaa=git add --all
+gc=git commit
 gcm=git commit -m $*
 gca=git commit --amend
-gcaa=git commit -A --amend
-addcom=git add -A && git commit -m $*
-save=git add -A && git commit -m 'Work in progress'
+amend=git add --all && git commit --all
+commit=git add --all && git commit -m $*
+save=git add --all && git commit -m 'Work-in-progress'
+gpp=git pull && git push
+:: Undo
+nah=git checkout .
+gres=git reset
+gresh=git reset HEAD --hard
+undo-commit=git reset HEAD~1
+grev=git revert
+gcln=git clean -fd
+gcln-all=git clean -fdx
+:: Stash
 gist=git stash
 gisl=git stash list
 gisa=git stash apply
@@ -46,25 +44,26 @@ gisp=git stash pop
 gisp-sel=git stash pop stash@{$*}
 gisb=git stash branch $*
 giss=git stash show -p stash@{$*}
+:: Branching
 gdev=git checkout develop
 gmas=git checkout master
 gica=git checkout $*
 gicb=git checkout -b $*
 gmer=git merge --no-ff $*
-gres=git reset
-gresh=git reset HEAD --hard
-grev=git revert
-gcln=git clean -fd
-gcln-all=git clean -fdx
 gb=git branch
 branchcd=git branch --sort=-committerdate
 branchcdr=git for-each-ref --sort=-committerdate refs/heads/
+:: Logging
 gl=git log --oneline --graph --decorate  $*
 gll=git log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat
 dog=git log --all --decorate --oneline --graph
 wdiff=git diff --color-words
 gfl=git log -u
+:: Rebase
 grem=git rebase master
 gred=git rebase develop
-greb=git rebase $* 
+greb=git rebase $*
+
+
+
 
